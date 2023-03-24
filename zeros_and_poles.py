a4e9484fef8c26f78612a3b58feb84946891c631
Luca Giaccone <luca.giaccone@polito.it>
2023-03-24 18:26:42 +0100

    changes aspects of the figures

--- zeros_and_poles.py
+++ zeros_and_poles.py
@@ -30,8 +30,8 @@ phase2 = real_pole(ft, k, f)
 fig1, axs = plt.subplots()
 phase1_icnirp = np.zeros_like(phase1)
 phase1_icnirp[f >= ft] = 90 * k
-axs.semilogx(f, phase1, linewidth=3, label='proposed')
-axs.semilogx(f, phase1_icnirp, 'k--', label='ICNIRP')
+axs.semilogx(f, phase1_icnirp, 'C1', linewidth=3, label='frequency')
+axs.semilogx(f, phase1, 'C2--', linewidth=3,label='frequency proposed')
 axs.set_xticks((ft*0.1, ft, ft*10))
 axs.set_xticklabels(("$0.1 f_j$", "$f_j$", "$10 f_j$"), fontsize=16)
 axs.set_ylim(-200,200)
@@ -40,13 +40,14 @@ axs.set_yticklabels(("-90 $k$", "0", "45 $k$", "90 $k$"), fontsize=16)
 axs.grid(linestyle='dashed')
 axs.set_xlabel("frequency (Hz)", fontsize=16)
 axs.set_ylabel("phase (deg)", fontsize=16)
+plt.legend(fontsize=16)
 plt.tight_layout()
 
 fig2, axs = plt.subplots()
 phase2_icnirp = np.zeros_like(phase2)
 phase2_icnirp[f >= ft] = -90 * k
-axs.semilogx(f, phase2, linewidth=3, label='proposed')
-axs.semilogx(f, phase2_icnirp, 'k--', label='ICNIRP')
+axs.semilogx(f, phase2_icnirp, 'C1', linewidth=3, label='frequency')
+axs.semilogx(f, phase2, 'C2--', linewidth=3, label='frequency proposed')
 axs.set_xticks((ft*0.1, ft, ft*10))
 axs.set_xticklabels(("$0.1 f_j$", "$f_j$", "$10 f_j$"), fontsize=16)
 axs.set_ylim(-200,200)
@@ -55,12 +56,11 @@ axs.set_yticklabels(("-90 $k$", "-45 $k$", "0", "90 $k$"), fontsize=16)
 axs.grid(linestyle='dashed')
 axs.set_xlabel("frequency (Hz)", fontsize=16)
 axs.set_ylabel("phase (deg)", fontsize=16)
-
+plt.legend(fontsize=16)
 plt.tight_layout()
+
+fig1.savefig("real_zero.png", dpi=300)
+fig2.savefig("real_pole.png", dpi=300)
 
 # show figures
 plt.show()
-
-# fig3.savefig("./img/real_zero.png", dpi=300)
-# fig4.savefig("./img/real_pole.png", dpi=300)
-
